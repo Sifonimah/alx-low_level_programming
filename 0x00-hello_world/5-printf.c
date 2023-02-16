fdc8b6342ba844910f8b2901b69ebaff36948768
Sifonimah <sifonimaikop@gmail.com>
2023-02-16 07:42:46 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A prrogramm that print a line using the print function
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+	Return (0);
+}
+
