a6791a99c139025a5bbb5af128c0968ec437e36a
Sifonimah <sifonimaikop@gmail.com>
2023-02-17 07:14:43 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+ /** 
+ *main - this is the exit point 
+ *Description: the program use while loop to print a-z 
+ *Return: is 0 
+ */ 
+ int main(void) 
+ { 
+ int i = 'a'; 
+ while (i <= 'z') 
+ { 
+ putchar(i); 
+ i++; 
+ } 
+ putchar('\n'); 
+ return (0); 
+ }
