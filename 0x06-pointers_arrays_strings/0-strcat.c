912de62bcd241a7c13c0b1e7a7a22080b47bbabf
Sifonimah <sifonimaikop@gmail.com>
2023-03-05 07:01:50 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,39 +10,24 @@
 char *_strcat(char *dest, char *src)
 
 {
+	int i;
+	int j;
 
-        int i;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
 
-        int j;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-
-        i = 0;
-
-        while (dest[i] != '\0')
-
-        {
-
-                i++;
-
-        }
-
-        j = 0;
-
-        while (src[j] != '\0')
-
-        {
-
-                dest[i] = src[j];
-
-                i++;
-
-                j++;
-
-        }
-
-
-        dest[i] = '\0';
-
-        return (dest);
+	dest[i] = '\0';
+	return (dest);
 
 }
